f6e6f13a51c3629e615b72fed50b028e74203eda
fchimpan <ryo-mimura@cybozu.co.jp>
2022-10-21 02:09:04 +0900

    feat: Changed to obtain options from profile

---
+++ config/config.go
@@ -0,0 +1,22 @@
+package config
+
+import "gopkg.in/ini.v1"
+
+type Config struct {
+	SpaceKey string `ini:"space_key"`
+	Domain   string `ini:"domain"`
+	Parent   string `ini:"parent"`
+}
+
+func GetConfig(configFilePath, sectionName string) (*Config, error) {
+	c, err := ini.Load(configFilePath)
+	if err != nil {
+		return nil, err
+	}
+	cfg := &Config{
+		SpaceKey: c.Section(sectionName).Key("space_key").String(),
+		Domain:   c.Section(sectionName).Key("domain").String(),
+		Parent:   c.Section(sectionName).Key("parent").String(),
+	}
+	return cfg, nil
+}
